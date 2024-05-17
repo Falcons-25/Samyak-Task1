6d8d0a7923c89a8138dd8d5a27584f87f5972777
Samyak Doshi <71555137+samyakdoshi5@users.noreply.github.com>
2024-05-18 01:16:04 +0530

    Update ultrasonic_out.ino

--- ultrasonic_out.ino
+++ ultrasonic_out.ino
@@ -1,28 +1,31 @@
-const int trigPin = 9;
-const int echoPin = 10;
-// defines variables
+const int trigPin = 9; //trigger pin
+const int echoPin = 10; // echo pin
+
+// define variables 
 long duration;
 float distance;
 void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-  Serial.begin(9600); // Starts the serial communication
+  Serial.begin(9600); // Starts the serial communication at 9600 baud rate
 }
 void loop() {
   // Clears the trigPin
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
+  
   // Sets the trigPin on HIGH state for 10 micro seconds
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
+  
   // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
+  
   // Calculating the distance
   distance = duration * 0.001/2 ;
+  
   // Prints the distance on the Serial Monitor
-  // Serial.print(millis());
-  // Serial.print(",");
   Serial.print(distance);
   Serial.print("\n");
 
